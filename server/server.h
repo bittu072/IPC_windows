4a2332a48fa05ffe954085e563265a4a144b0247
bittu072 <bittu@Brindals-MacBook-Pro.local>
2018-07-08 03:58:17 -0700

    updated the code of server + added diff methods which client can use to save or get the data from server

--- server/server.h
+++ server/server.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "stdafx.h"
 
+#include <windows.h>
+#include <tchar.h>
+#include <strsafe.h>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+
+#include <string>
+#include <vector>
+
+#define BUF_SIZE 1024
 
 class SetupServer
 {
@@ -11,9 +23,16 @@ public:
 private:
 	std::string response_to_client(std::string recv_msg);
 	void write_to_client(std::string);
+	std::string getusename();
+	void save_list(std::string list_str);
+	std::string get_list();
+	std::string get_elem_from_list(std::string elem_ind);
+	std::string update_elem_in_list(std::string recv_substr);
 
 	HANDLE pipe_handle;
 	char buf[BUF_SIZE];
 	DWORD read_dw;
 	DWORD write_dw;
-}
+	std::string last_msg;
+	std::vector<std::string> list_from_client;
+};
